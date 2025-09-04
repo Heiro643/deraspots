c084d2d7188ad84d902ff2312eb08b72e5bcb4a6
Narisada1221 <241205134@ccmailg.meijo-u.ac.jp>
2025-09-05 00:02:41 +0900

    PARCO画面作成 ・TextView ・PARCO画像 ・mapボタン

--- app/src/main/java/jp/ac/meijou/android/deraspots/MainActivity4.java
+++ app/src/main/java/jp/ac/meijou/android/deraspots/MainActivity4.java
@@ -1,6 +1,10 @@
 package jp.ac.meijou.android.deraspots;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.deraspots.databinding.ActivityMain4Binding;
+
 public class MainActivity4 extends AppCompatActivity {
+
+    private ActivityMain4Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main4);
+        binding = ActivityMain4Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.mapButton.setOnClickListener(v -> {
+            Uri gmmIntentUri = Uri.parse("geo:35.1635,136.9086?q=名古屋パルコ");
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+            startActivity(mapIntent);
+        });
+
     }
 }
