9ce34e1a2e21928b4537c304ddbb9ae43a526728
清古拓聖 <241205095@ccmailg.meijo-u.ac.jp>
2025-09-05 11:09:04 +0900

    水族館用のViewを作成し，bindingを用いてViewを立ち上げられるようにしました

--- app/src/main/java/jp/ac/meijou/android/deraspots/MainActivity5.java
+++ app/src/main/java/jp/ac/meijou/android/deraspots/MainActivity5.java
@@ -8,12 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.deraspots.databinding.ActivityMain5Binding;
+
 public class MainActivity5 extends AppCompatActivity {
 
+    private ActivityMain5Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        binding = ActivityMain5Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         setContentView(R.layout.activity_main5);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
